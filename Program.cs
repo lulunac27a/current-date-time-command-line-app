f0300a9086523ed54dd8de9c3c350d337a558209
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-16 17:18:29 -0500

    Add more date and time methods in `DateTime` struct

--- Program.cs
+++ Program.cs
@@ -36,5 +36,23 @@ static class Program
         Console.WriteLine(
             "Current time zone offset: " + now.ToString("zz") + " (" + now.ToString("zzz") + ")"
         );
+        Console.WriteLine("Current date and time using DateTime properties:");
+        Console.WriteLine("Current date: " + now.Date);
+        Console.WriteLine("Current day of month: " + now.Day);
+        Console.WriteLine("Current day of week: " + now.DayOfWeek);
+        Console.WriteLine("Current day of year: " + now.DayOfYear);
+        Console.WriteLine("Current hour: " + now.Hour);
+        Console.WriteLine("Current kind: " + now.Kind);
+        Console.WriteLine("Current microsecond: " + now.Microsecond);
+        Console.WriteLine("Current millisecond: " + now.Millisecond);
+        Console.WriteLine("Current minute: " + now.Minute);
+        Console.WriteLine("Current month: " + now.Month);
+        Console.WriteLine("Current nanosecond: " + now.Nanosecond);
+        Console.WriteLine("Current second: " + now.Second);
+        Console.WriteLine("Current ticks: " + now.Ticks);
+        Console.WriteLine("Current time of day: " + now.TimeOfDay);
+        Console.WriteLine("Today's date: " + DateTime.Today);
+        Console.WriteLine("Current UTC date and time: " + DateTime.UtcNow);
+        Console.WriteLine("Current year: " + now.Year);
     }
 }

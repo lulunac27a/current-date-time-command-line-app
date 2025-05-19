bacefacd8d8638295dc9ed9c4b958e11a4f21de5
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-19 15:38:41 -0500

    Change order of outputted date and time values

--- Program.cs
+++ Program.cs
@@ -30,23 +30,9 @@ namespace current_date_time_command_line_app
             Console.WriteLine("Year month: " + now.ToString("y") + " (" + now.ToString("Y") + ")");
             Console.WriteLine("Other formats:");
             Console.WriteLine(
-                "Current day of month: "
-                    + now.ToString("dd")
-                    + " ("
-                    + now.ToString("ddd")
-                    + ")"
-                    + " ("
-                    + now.ToString("dddd")
-                    + ")"
-            );
-            Console.WriteLine("Current part of second: " + now.ToString("fffffff"));
-            Console.WriteLine(
                 "Current era: " + now.ToString("g ") + " (" + now.ToString("gg") + ")"
             );
-            Console.WriteLine(
-                "Current hour: " + now.ToString("hh") + " (" + now.ToString("HH") + ")"
-            );
-            Console.WriteLine("Current minute: " + now.ToString("mm"));
+            Console.WriteLine("Current year: " + now.ToString("yyyy") + ")");
             Console.WriteLine(
                 "Current month: "
                     + now.ToString("MM")
@@ -57,32 +43,46 @@ namespace current_date_time_command_line_app
                     + now.ToString("MMMM")
                     + ")"
             );
+            Console.WriteLine(
+                "Current day of month: "
+                    + now.ToString("dd")
+                    + " ("
+                    + now.ToString("ddd")
+                    + ")"
+                    + " ("
+                    + now.ToString("dddd")
+                    + ")"
+            );
+            Console.WriteLine(
+                "Current hour: " + now.ToString("hh") + " (" + now.ToString("HH") + ")"
+            );
+            Console.WriteLine("Current minute: " + now.ToString("mm"));
             Console.WriteLine("Current second: " + now.ToString("s "));
+            Console.WriteLine("Current part of second: " + now.ToString("fffffff"));
             Console.WriteLine(
                 "Current AM/PM: " + now.ToString("t ") + " (" + now.ToString("tt") + ")"
             );
-            Console.WriteLine("Current year: " + now.ToString("yyyy") + ")");
             Console.WriteLine(
                 "Current time zone offset: " + now.ToString("zz") + " (" + now.ToString("zzz") + ")"
             );
             Console.WriteLine("Current date and time using DateTime properties:");
-            Console.WriteLine("Current date: " + now.Date);
+            Console.WriteLine("Current year: " + now.Year);
+            Console.WriteLine("Current month: " + now.Month);
             Console.WriteLine("Current day of month: " + now.Day);
             Console.WriteLine("Current day of week: " + now.DayOfWeek);
             Console.WriteLine("Current day of year: " + now.DayOfYear);
+            Console.WriteLine("Current date: " + now.Date);
+            Console.WriteLine("Today's date: " + DateTime.Today);
             Console.WriteLine("Current hour: " + now.Hour);
-            Console.WriteLine("Current kind of time: " + now.Kind);
-            Console.WriteLine("Current microsecond: " + now.Microsecond);
-            Console.WriteLine("Current millisecond: " + now.Millisecond);
             Console.WriteLine("Current minute: " + now.Minute);
-            Console.WriteLine("Current month: " + now.Month);
-            Console.WriteLine("Current nanosecond: " + now.Nanosecond);
             Console.WriteLine("Current second: " + now.Second);
+            Console.WriteLine("Current millisecond: " + now.Millisecond);
+            Console.WriteLine("Current microsecond: " + now.Microsecond);
+            Console.WriteLine("Current nanosecond: " + now.Nanosecond);
+            Console.WriteLine("Current kind of time: " + now.Kind);
             Console.WriteLine("Current ticks: " + now.Ticks);
             Console.WriteLine("Current time of day: " + now.TimeOfDay);
-            Console.WriteLine("Today's date: " + DateTime.Today);
             Console.WriteLine("Current UTC date and time: " + DateTime.UtcNow);
-            Console.WriteLine("Current year: " + now.Year);
         }
     }
 }

3fb66fb27106c7c2fbc70397665b6b001d6dee35
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-22 17:38:00 -0500

    Add another type hint

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let package = Package(
+let package: Package = Package(
     name: "current-date-time-command-line-app",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.

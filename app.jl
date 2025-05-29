dab7e8ba93f16c2d489bcf0df0815eba168b4456
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-05-28 20:16:23 -0500

    Add comments

--- app.jl
+++ app.jl
@@ -1,9 +1,11 @@
 using Dates
-today = Dates.today()
-now = Dates.now()
+today = Dates.today()#today's date
+now = Dates.now()#current date and time
 println("Today's date: ", today)
 println("Current date and time: ", now)
 println("Current date and time in all supported formats:")
+#print current date and time in all supported formats
+#format the date and time values with a specified pattern for all supported formats
 println("Standard date and time formats:")
 println("Current year: ", Dates.format(now, "Y"))
 println("Current month: ", Dates.format(now, "m"), " (", Dates.format(now, "u"), ")", " (", Dates.format(now, "U"), ")")
